5b60d94ef75b2c590c931a083cd3fa89b20be10e
Arpit Vaghela <arpit74057@gmail.com>
2023-08-12 18:31:41 +0530

    Init the project

---
+++ frontend/src/components/ComponentOne.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ComponentOne = (props)=>{
+    
+    return(
+        <>
+            <h1>In ComponentOne !</h1>
+        </>
+    );
+}
+
+export default ComponentOne;
